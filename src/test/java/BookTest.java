7b669700a962cb643b3afd618021cf4aacd50ca2
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2024-11-18 09:54:49 +0100

    Initial commit

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+	//G
+	@Test
+	public void test2EqualBooks() {
+
+	}
+
+	//G
+	@Test
+	public void test2NonEqualBooks() {
+
+	}
+
+}
---
+++ src/test/java/LoanTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class LoanTest {
+
+	//VG
+	@Test
+	public void testDueDate() {
+
+   }
+}
